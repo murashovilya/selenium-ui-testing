69077e3c041fdea0476332e230c12759ac08e516
sdet0mis <sdet0mis@mail.ru>
2025-10-14 16:24:23 +0300

    Update authentication page and test
    Remove noqa comments Update locators and step names to English Add
    severity level to test Improve assertion formatting

--- pages/authentication_page.py
+++ pages/authentication_page.py
@@ -9,19 +9,19 @@ from pages.base_page import BasePage
 class AuthenticationPage(BasePage):
     def __init__(self, driver: WebDriver) -> None:
         super().__init__(driver)
-        self.URL = "https://www.httpwatch.com/httpgallery/authentication/#showExample10"  # noqa
+        self.URL = "https://www.httpwatch.com/httpgallery/authentication/#showExample10"
         self.USERNAME = "httpwatch"
         self.PASSWORD = "httpwatch"
         self.CREDENTIALS = f"{self.USERNAME}:{self.PASSWORD}"
         self.DISPLAY_IMAGE_BUTTON = (By.ID, "displayImage")
         self.IMAGE_URL = f"https://{self.CREDENTIALS}@www.httpwatch.com/httpgallery/authentication/authenticatedimage/default.aspx"  # noqa
-        self.AUTHENTICATED_IMAGE = (By.XPATH, "//img")
+        self.AUTHENTICATED_IMAGE = (By.TAG_NAME, "img")
 
-    @allure.step("Нажать на кнопку Display Image")
+    @allure.step("Click Display Image Button")
     def click_display_image_button(self) -> None:
         self.click(self.DISPLAY_IMAGE_BUTTON)
 
-    @allure.step("Открыть страницу картинки")
+    @allure.step("Open Image Page")
     def open_image_page(self) -> None:
         self.driver.get(self.IMAGE_URL)
 
--- tests/test_authentication_page.py
+++ tests/test_authentication_page.py
@@ -5,17 +5,18 @@ from pages.authentication_page import AuthenticationPage
 
 
 @allure.epic("UI")
-@allure.feature("Страница авторизации")
+@allure.feature("Authentication page")
+@allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.smoke
 class TestAuthenticationPage:
-    @allure.title("Авторизация")
-    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.title("Authentication")
     def test_authentication(self, authentication_page: AuthenticationPage):
         authentication_page.click_display_image_button()
         authentication_page.open_image_page()
         authentication_page.authenticated_image_is_displayed()
-        image_attribute = (
+        image_attribute = str(
             authentication_page.find_authenticated_image().get_attribute("src")
         )
-        assert authentication_page.CREDENTIALS in image_attribute, \
-            f"Некорректная картинка: {image_attribute}"
+        assert authentication_page.CREDENTIALS in image_attribute, (
+            f"Incorrect image: {image_attribute}"
+        )
